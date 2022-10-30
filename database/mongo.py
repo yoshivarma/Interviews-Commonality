b703cf978837d3e335ab195990cd89b1289b2bc1
Aditi Garg <aditigarg@MacBook-Air.local>
2022-10-30 08:49:16 -0400

    Adding code and experiments for case CRUDs, todo - recording CRUD

--- database/mongo.py
+++ database/mongo.py
@@ -3,31 +3,33 @@ from models.case import Case, Recording
 
 from urllib.parse import quote_plus
 from motor.motor_asyncio import AsyncIOMotorClient
-from odmantic import AIOEngine
+from odmantic import AIOEngine, exceptions
 
 engine = None
 
-def init(host, user, password, database):
+async def init(host, user, password, database):
 	global engine
 	uri = "mongodb://%s:%s@%s" % (quote_plus(user), quote_plus(password), host)
 	client = AsyncIOMotorClient(uri)
 	engine = AIOEngine(client=client, database=database)
-	return engine.configure_database([Case])
+	await engine.configure_database([Case]) 
 
-def getAllCases():
-	pass
+async def getAllCases():
+	cases = await engine.find(Case)
+	return cases
 
-def createCase(case: Case):
-	return engine.save(case)
+async def createCase(case: Case):
+	try:
+		await engine.save(case)
+	except exceptions.DuplicateKeyError:
+		print("Case could not be added as a case with the same name exists.")
 
-def getCase():
-	pass
+async def getCase(case_id: str):
+	case = await engine.find(Case, Case.id == case_id)
+	return(case)
 
-def updateCase():
-	pass
-
-def deleteCase():
-	pass
+async def deleteCase(case_id: str):
+	await engine.remove(Case, Case.id == case_id)
 
 def createRecording():
 	pass
--- database/testAllEndpoints.py
+++ database/testAllEndpoints.py
@@ -9,8 +9,58 @@ password = "rootpass"
 database = "cases"
 
 async def test():
+	
 	await mongo.init(host, user, password, database)
-	case = mongo.Case(name="Ronnie Abduction", recordings=[mongo.Recording(name="Anna Statement", recording_link="https://www.google.com")])
-	await mongo.createCase(case)
+
+	# Add cases
+	print("Adding cases: ")
+	
+	case1 = mongo.Case(name="New York Abduction", recordings=
+		[
+			mongo.Recording(name="Anna Statement",recording_link="https://docs.google.com/document/case1rec1dummyURL")
+		])
+	print("Adding to DB: {0}".format(case1))
+	await mongo.createCase(case1)
+
+	case2 = mongo.Case(name="LA Murder", recordings=
+		[
+			mongo.Recording(name="Witness A Statement",recording_link="https://docs.google.com/document/case2rec1dummyURL"),
+			mongo.Recording(name="Witness B Statement",recording_link="https://docs.google.com/document/case2rec2dummyURL")
+		])
+	print("\nAdding to DB: {0}".format(case2))
+	await mongo.createCase(case2)
+
+
+	# Reading all cases
+	print("\nReading all cases")
+	cases = await mongo.getAllCases()
+	print(cases)
+
+
+	# Deleting a case
+	print("\nDeleting NY Abduction case using case ID")
+	case_to_delete = list(filter(lambda case : case.name == "New York Abduction", cases))[0]
+	await(mongo.deleteCase(case_to_delete.id))
+
+
+	# Reading cases post deletion
+	print("\nReading cases post deletion")
+	cases = await mongo.getAllCases()
+	print(cases)
+
+
+	print("\nRe-adding NY Abduction case")
+	await mongo.createCase(
+		mongo.Case(name="New York Abduction", recordings=
+		[
+			mongo.Recording(name="Anna Statement",recording_link="https://docs.google.com/document/case1rec1dummyURL")
+		])
+	)
+
+	# Retrieving a case by case ID
+	print("\nRetrieving a case by case ID")
+	cases = await mongo.getAllCases()
+	case = await mongo.getCase(cases[1].id)
+	print(case)
 
 asyncio.run(test())
