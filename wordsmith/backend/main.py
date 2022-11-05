29c50c9df7b0364cd93cf7974a348bd0c5c92d61
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2022-11-05 01:14:02 -0400

    Audio sent through post request

--- wordsmith/backend/main.py
+++ wordsmith/backend/main.py
@@ -1,12 +1,17 @@
 from typing import Union, List
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
-from model.model import find_keywords_model
-from fastapi.middleware.cors import CORSMiddleware
+# from model.model import find_keywords_model
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["*"]
+origins = [
+    "http://localhost:3000/",
+    "http://localhost:8080/",
+]
+
+origins = ['*']
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,27 +25,36 @@ app.add_middleware(
 class Recording(BaseModel):
     id: int
     name: str
-    keywords: list
+    link: str
 
 # class Case(BaseModel):
 #     id: int
 #     name: str
 #     recordings: list(Recordings)
 
+
 DB: List[Recording] = [
-    Recording(id=1, name="Recording 1", keywords=['word 1', 'word 2']),
-    Recording(id=2, name="Recording 2", keywords=['word 1', 'word 2' , 'word3'])
+    Recording(id=1, name="Recording 1", link="abcdef"),
+    Recording(id=2, name="Recording 2", link="abcefsdf")
 ]
 
-@app.get("/api")
-def read_root():
-    keywords = find_keywords_model()
-    return keywords
+# @app.get("/api")
+# def read_root():
+#     keywords = find_keywords_model()
+#     return keywords
 
-#@app.post("/keywords")
-#def find_keywords():
-  #  keywords = find_keywords_model()
- #   return {"keywords": keywords}
+# @app.post("/keywords")
+# def find_keywords():
+#  keywords = find_keywords_model()
+#   return {"keywords": keywords}
+
+
+@app.post("/findkeywords")
+async def find_keywords(audios = List[UploadFile]):
+    # for files in audios:
+    #     print('Help')
+    return [a for a in [1, 2, 3, 3, 4, 4]]
+    # return {"filenames": audios}
 
 
 @app.get("/items/{item_id}")
--- wordsmith/frontend/src/App.js
+++ wordsmith/frontend/src/App.js
@@ -11,127 +11,198 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ReactAudioPlayer from "react-audio-player";
-import "./App.css"; 
-import axios from "axios"
+import "./App.css";
+import axios from "axios";
 
-function App() {
-	const [recording, setRecording] = useState([]);
+// function App() {
+// 	const [recording, setRecording] = useState([]);
 
-	useEffect(() => {
-		axios.get("http://localhost:8000/api").then((res) => setRecording(res.data));
-	}, []);
-	return recording.map((r, index) => {
-		return <p key={index}>
-			{/* {r.id} {r.name} {r.keywords} */}
-			{r}
-		</p>;
-	});
-	// return {recording}
-}
+// 	useEffect(() => {
+// 		axios.get("/api").then((res) => setRecording(res.data));
+// 	}, []);
+// 	return recording.map((r, index) => {
+// 		return <p key={index}>
+// 			{r.id} {r.name} {r.keywords}
+// 		</p>;
+// 	});
+// }
 
-// const App = () => {
-// 	const [audio, setAudio] = useState("");
-// 	const [audios, setAudios] = useState([]);
-// 	const [audioName, setAudioName] = useState([]);
+const App = () => {
+	const [audio, setAudio] = useState("");
+	const [audios, setAudios] = useState([]);
+	const [audioName, setAudioName] = useState([]);
+	const [keywords, setKeywords] = useState([]);
 
-// 	let selected_audios = [];
-// 	let selected_audio_names = [];
+	let selected_audios = [];
+	let selected_audio_names = [];
 
-// 	const addFile = (e) => {
-// 		Array.from(e.target.files).forEach((file) => {
-// 			if (file) {
-// 				selected_audios.push(URL.createObjectURL(file));
-// 				selected_audio_names.push(file.name);
-// 				setAudio(URL.createObjectURL(file));
-// 			}
-// 		});
+	const addFile = (e) => {
+		Array.from(e.target.files).forEach((file) => {
+			if (file) {
+				selected_audios.push(URL.createObjectURL(file));
+				selected_audio_names.push(file.name);
+				setAudio(URL.createObjectURL(file));
+			}
+		});
 
-// 		setAudios([...audios, selected_audios]);
-// 		setAudioName([...audioName, selected_audio_names]);
-// 	};
+		setAudios([...audios, selected_audios]);
+		setAudioName([...audioName, selected_audio_names]);
+	};
 
-// 	return (
-// 		<Container fixed>
-// 			<Box
-// 				component="span"
-// 				sx={{
-// 					"& > :not(style)": { m: 1 },
-// 				}}
-// 				noValidate
-// 				autoComplete="off"
-// 			>
-// 				<div>
-// 					{/* <TextField
-// 						id="outlined-basic"
-// 						label=""
-// 						variant="outlined"
-// 						sx={{ width: "200px"}}
-// 					/>
+	const findKeywords = async () => {
+		const formData = new FormData();
 
-// 					<Button
-// 						variant="contained"
-// 						component="label"
-// 					>
-// 						<RadioButtonCheckedIcon />
-// 					</Button> */}
-// 					<Button variant="contained" component="label">
-// 						<AddIcon />
-// 						<input type="file" onChange={addFile} accept=".mp3, .wav" hidden />
-// 					</Button>
-// 				</div>
-// 			</Box>
-// 			<Box>
-// 				<List
-// 					sx={{
-// 						width: "100%",
-// 						maxWidth: 360,
-// 						bgcolor: "background.deepPurple[500]",
-// 						position: "relative",
-// 						overflow: "auto",
-// 						maxHeight: 300,
-// 						"& ul": { padding: 0 },
-// 					}}
-// 					subheader={<li />}
-// 				>
-// 					{audios.map((a, index) => {
-// 						return (
-// 							<li key={index}>
-// 								<ul>
-// 									<ListItem onClick={() => setAudio(a.toString())}>
-// 										<ListItemButton>
-// 											<ListItemText primary={audioName[index]} />
-// 										</ListItemButton>
-// 									</ListItem>
-// 								</ul>
-// 							</li>
-// 						);
-// 					})}
-// 				</List>
-// 			</Box>
+		audios.forEach((ad) => {
+			formData.append("audios", ad);
+		});
 
-// 			<Box
-// 				component="span"
-// 				sx={{
-// 					"& > :not(style)": { m: 1 },
-// 				}}
-// 				noValidate
-// 				autoComplete="off"
-// 			>
-// 				<ReactAudioPlayer src={audio} autoPlay controls />
-// 			</Box>
-// 			<Box
-// 				component="span"
-// 				sx={{
-// 					"& > :not(style)": { m: 1 },
-// 				}}
-// 				noValidate
-// 				autoComplete="off"
-// 			>
-// 				<Button variant="contained">Find Keywords</Button>
-// 			</Box>
-// 		</Container>
-// 	);
-// };
+		const config = {
+			headers: {
+				"content-type": "multipart/form-data",
+				"Access-Control-Allow-Origin": "*",
+			},
+		};
+		try {
+			let result = await axios.post(
+				"http://localhost:8000/findkeywords",
+				formData,
+				config
+			);
+			setKeywords([...keywords, result.data])
+			console.log(result.data);
+		} catch (error) {
+			console.error(error); // NOTE - use "error.response.data` (not "error")
+		}
+
+		// const requestOptions = {
+		// 	method: "POST",
+		// 	body: formData,
+		// };
+
+		// fetch("http://localhost:8000/findkeywords", requestOptions)
+		// 	.then((response) => response.json())
+		// 	.then(function (response) {
+		// 		console.log(response);
+		// 	});
+	};
+
+	return (
+		<Container fixed>
+			<form>
+				<Box
+					component="span"
+					sx={{
+						"& > :not(style)": { m: 1 },
+					}}
+					noValidate
+					autoComplete="off"
+				>
+					<div>
+						{/* <TextField
+						id="outlined-basic"
+						label=""
+						variant="outlined"
+						sx={{ width: "200px"}}
+					/>
+
+					<Button
+						variant="contained"
+						component="label"
+					>
+						<RadioButtonCheckedIcon />
+					</Button> */}
+						<Button variant="contained" component="label">
+							<AddIcon />
+							<input
+								type="file"
+								onChange={addFile}
+								accept=".mp3, .wav"
+								hidden
+							/>
+						</Button>
+					</div>
+				</Box>
+				<Box>
+					<List
+						sx={{
+							width: "100%",
+							maxWidth: 360,
+							bgcolor: "background.deepPurple[500]",
+							position: "relative",
+							overflow: "auto",
+							maxHeight: 300,
+							"& ul": { padding: 0 },
+						}}
+						subheader={<li />}
+					>
+						{keywords.map((a, index) => {
+							return (
+								<li key={index}>
+									<ul>
+										<ListItem>
+												<ListItemText primary={a} />
+										</ListItem>
+									</ul>
+								</li>
+							);
+						})}
+					</List>
+				</Box>
+				<Box>
+					<List
+						sx={{
+							width: "100%",
+							maxWidth: 360,
+							bgcolor: "background.deepPurple[500]",
+							position: "relative",
+							overflow: "auto",
+							maxHeight: 300,
+							"& ul": { padding: 0 },
+						}}
+						subheader={<li />}
+					>
+						{audios.map((a, index) => {
+							return (
+								<li key={index}>
+									<ul>
+										<ListItem onClick={() => setAudio(a.toString())}>
+											<ListItemButton>
+												<ListItemText primary={audioName[index]} />
+											</ListItemButton>
+										</ListItem>
+									</ul>
+								</li>
+							);
+						})}
+					</List>
+				</Box>
+
+				<Box
+					component="span"
+					sx={{
+						"& > :not(style)": { m: 1 },
+					}}
+					noValidate
+					autoComplete="off"
+				>
+					<ReactAudioPlayer src={audio} autoPlay controls />
+				</Box>
+				<Box
+					component="span"
+					sx={{
+						"& > :not(style)": { m: 1 },
+					}}
+					noValidate
+					autoComplete="off"
+				>
+					<Button variant="contained" onClick={() => findKeywords()}>
+						Find Keywords
+					</Button>
+				</Box>
+			</form>
+		</Container>
+	);
+};
 export default App;
